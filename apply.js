9aa32f6aa5ab6edb498dbcea48b46a28e02b8f49
MrCookie <110797323+invixitydev@users.noreply.github.com>
2023-03-04 17:35:43 -0800

    initial commit

---
+++ apply.js
@@ -0,0 +1,11 @@
+const style = document.createElement('style');
+style.innerHTML = `
+body { 
+  font-family: "JetBrains Mono", "Jetbrains Mono Nerd", "JetBrainsMono Nerd Font"; 
+}
+
+textarea, input, label {
+  font-family: inherit;
+}
+`;
+document.body.appendChild(style);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Jetbrains Mono Scratch
+A Firefox userstyle to make the font Jetbrains Mono.
+
+## License
+This is licensed under the [GPLv3.0](https://gnu.org/licenses/gpl-3.0), for more info, see `license.md`.
